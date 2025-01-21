a712da109ce12da4085a3c1c0e3fa09eaf2acbc5
devuri <devuri@users.noreply.github.com>
2025-01-21 11:41:53 -0500

    chore: fix tests

--- tests/Unit/ContainerExampleTest.php
+++ tests/Unit/ContainerExampleTest.php
@@ -18,7 +18,7 @@ use PHPUnit\Framework\TestCase;
  *
  * @coversNothing
  */
-class ExampleTest extends TestCase
+class ContainerExampleTest extends TestCase
 {
     public function test_example_true_is_true(): void
     {
